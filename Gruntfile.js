a98a87b63fee74a02c3284bfc4c81dcae916a17a
tobiasbu <flamenco.bluegrass@gmail.com>
2016-05-24 17:03:56 -0300

    npm test script added

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,11 +14,18 @@ module.exports = function(grunt) {
   //grunt.registerTask('default', ['uglify']);
 
   // push-pages
-  grunt.task.registerTask('push-pages', function(){
+  grunt.registerTask('push-pages', function(){
       var shell = require('shelljs');
+
+      var branch = 'gh-pages';
+
+      grunt.log.write("Deploy builded site to " + branch + ":\n");
+
+       shell.exec("git checkout gh-pages");
+       shell.exec("git rm -rf .");
        shell.exec('git subtree split --prefix docs -b gh-pages');
        shell.exec('git push -f origin gh-pages:gh-pages');
-       shell.exec('git branch -D gh-pages')
+       //shell.exec('git branch -D gh-pages')
    });
 
 };
