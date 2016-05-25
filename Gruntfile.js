fcaffa7e22b436aff1de01da3bc22b3db67ba48a
tobiasbu <flamenco.bluegrass@gmail.com>
2016-05-25 14:34:28 -0300

    Post-CSS add to gruntfile. Finally starting to create the website

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,16 +5,42 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    postcss: {
+        options: {
+        /*  map: true, // inline sourcemaps
+
+          // or
+          map: {
+              inline: false, // save all sourcemaps as separate files...
+              annotation: 'dist/css/maps/' // ...to the specified directory
+          },*/
+
+          processors: [
+            //require('pixrem')(), // add fallbacks for rem units
+            require('postcss-import')(),
+            //require('postcss-cssnext')({browsers: 'last 2 versions'}), // add vendor prefixes
+            require('autoprefixer')({browsers: ['last 2 versions']}),
+            require('postcss-font-magician')(),
+            require('cssnano')() // minify the result
+          ]
+        },
+        dist: {
+          src: 'srccss/main.css',//'css/*.css'
+          dest: '_includes/css/main.css'//'./css'
+        }
+      }
   });
 
   // Load the plugin that provides the "uglify" task.
   //grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-postcss');
 
   // Default task(s).
-  //grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['postcss']);
+
 
   // push-pages
-  grunt.registerTask('push-pages', function(){
+  /*grunt.registerTask('push-pages', function(){
       var shell = require('shelljs');
 
       var branch = 'gh-pages';
@@ -26,6 +52,6 @@ module.exports = function(grunt) {
        shell.exec('git subtree split --prefix docs -b gh-pages');
        shell.exec('git push -f origin gh-pages:gh-pages');
        //shell.exec('git branch -D gh-pages')
-   });
+   });*/
 
 };
